5bcd6b6784aa88e2bcf79ad9e91abad4aad240bb
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-15 11:39:19 -0400

    Cambios en el generador, ahora entrega la salida numérica en stock y precio artículo

--- generador/Generar.PrecioArticulos.XLSLISTA.cs
+++ generador/Generar.PrecioArticulos.XLSLISTA.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
 using System.IO;
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 
@@ -12,7 +13,6 @@ namespace Softech.Administrativo.Generacion
 {
     public static class XLSLIS
     {
-        #region Generar
         public static void Generar(DataSet datos, Dictionary<string, object> filtros, string rutaSalida, string nombreArchivo)
         {
             if (datos == null || datos.Tables.Count == 0)
@@ -30,114 +30,232 @@ namespace Softech.Administrativo.Generacion
             // Validar la insersión de imágenes
             // InsertarImagenesEnHoja(rutaArchivo, @"C:\EXPORTA");
 
-            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(rutaArchivo, true))
+            using (SpreadsheetDocument documento = SpreadsheetDocument.Open(rutaArchivo, true))
             {
-                WorkbookPart wbPart = doc.WorkbookPart;
-                WorksheetPart wsPart = ObtenerHoja(doc, "LISTA");
-                SheetData sheetData = wsPart.Worksheet.GetFirstChild<SheetData>();
+                int ultimaFila = 3;
 
-                // Elimina solo filas a partir de la fila 3 (sin borrar encabezados ni banners)
-                /*foreach (var row in sheetData.Elements<Row>())
-                {
-                    if (row.RowIndex.Value >= 3)
-                        row.Remove();
-                }*/
-
-                sheetData.RemoveAllChildren<Row>(); // Borra todas las filas
-                uint filaActual = 3;
-
-                // Obtener el número de registros
-                //int numeroRegistros = ObtenerNumeroRegistros(datos);
-
-                foreach (DataRow row in tabla.Rows)
-                {
-                    Row nuevaFila = new Row() { RowIndex = filaActual };
-
-                    nuevaFila.Append(
-                            CrearCeldaTexto("A", filaActual, row["co_art"].ToString()),
-                            CrearCeldaTexto("B", filaActual, row["art_des"].ToString()),
-                            //CrearCeldaTexto("C", filaActual, ObtenerReferencia(row)),
-                            CrearCeldaTexto("C", filaActual, row["co_cat"].ToString()),
-                            CrearCeldaTexto("D", filaActual, row["cat_des"].ToString()),
-                            CrearCeldaTexto("E", filaActual, row["Precio01"].ToString()),
-                            CrearCeldaTexto("F", filaActual, row["StockActual"].ToString()),
-                            CrearCeldaNumero("G", filaActual, 0),
-                            CrearCeldaTexto("H", filaActual, row["co_art"].ToString())
-                        );
-
-                    sheetData.AppendChild(nuevaFila);
-                    filaActual++;
-                }
-
-                // Guardar el documento
-                wsPart.Worksheet.Save();
+                Imprimir_02_datos_0(datos, rutaArchivo, nombreArchivo, documento, ref ultimaFila);
             }
         }
 
-        private static WorksheetPart ObtenerHoja(SpreadsheetDocument doc, string nombreHoja)
+        #region Imprimir
+        private static SpreadsheetDocument Imprimir_02_datos_0(DataSet dt, String ruta, String nombreArchivo, SpreadsheetDocument documento, ref int ultimaFila)
         {
-            foreach (Sheet hoja in doc.WorkbookPart.Workbook.Sheets)
-                if (hoja.Name == nombreHoja)
-                    return (WorksheetPart)doc.WorkbookPart.GetPartById(hoja.Id);
-
-            //throw new Exception($"La hoja '{nombreHoja}' no fue encontrada en el archivo.");
-            throw new Exception("La hoja " + nombreHoja + " no fue encontrada en el archivo.");
-        }
-
-        private static string ObtenerReferencia(DataRow row)
-        {
-            if (row.Table.Columns.Contains("modelo") && row["modelo"] != DBNull.Value)
-                return row["modelo"].ToString();
-            return row["co_uni"].ToString();
-        }
-
-        private static Cell CrearCeldaTexto(string columna, uint fila, string valor)
-        {
-            return new Cell
-            {
-                CellReference = columna + fila,
-                DataType = CellValues.String,
-                CellValue = new CellValue(valor ?? string.Empty)
-            };
-        }
-
-        private static Cell CrearCeldaNumero(string columna, uint fila, object valor)
-        {
-            decimal numero = 0;
-            if (valor != DBNull.Value)
-                decimal.TryParse(valor.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out numero);
-
-            return new Cell
-            {
-                CellReference = columna + fila,
-                DataType = CellValues.Number,
-                CellValue = new CellValue(numero.ToString(CultureInfo.InvariantCulture))
-            };
-        }
-
-        private static Cell CrearCeldaFormula(string columna, uint fila, string formula)
-        {
-            return new Cell
-            {
-                CellReference = columna + fila,
-                CellFormula = new CellFormula(formula),
-                DataType = CellValues.String
-            };
+            documento = Imprimir_02_02_renglones_0(dt, ruta, nombreArchivo, documento, ref ultimaFila);
+            return documento;
         }
         #endregion
 
-        #region Recuperar número de registros
-        private static int ObtenerNumeroRegistros(DataSet datos)
+        #region InsertCellInWorksheet
+
+        private static Cell InsertCellInWorksheet(Worksheet ws, string addressName)
         {
-            if (datos.Tables.Contains("MetaData"))
+
+            SheetData sheetData = ws.GetFirstChild<SheetData>();
+            Cell cell = null;
+
+            UInt32 rowNumber = GetRowIndex(addressName);
+            Row row = GetRow(sheetData, rowNumber);
+
+            // If the cell you need already exists, return it.
+            // If there is not a cell with the specified column name, insert one.  
+            Cell refCell = row.Elements<Cell>().
+                Where(c => c.CellReference.Value == addressName).FirstOrDefault();
+            if (refCell != null)
             {
-                DataTable metaData = datos.Tables["MetaData"];
-                if (metaData.Rows.Count > 0)
+                cell = refCell;
+            }
+            else
+            {
+                cell = CreateCell(row, addressName);
+            }
+            return cell;
+        }
+
+        #endregion
+
+        #region GetRowIndex
+
+        private static UInt32 GetRowIndex(string address)
+        {
+            string rowPart;
+            UInt32 l;
+            UInt32 result = 0;
+
+            for (int i = 0; i < address.Length; i++)
+            {
+                if (UInt32.TryParse(address.Substring(i, 1), out l))
                 {
-                    return Convert.ToInt32(metaData.Rows[0]["Registros"]);
+                    rowPart = address.Substring(i, address.Length - i);
+                    if (UInt32.TryParse(rowPart, out l))
+                    {
+                        result = l;
+                        break;
+                    }
                 }
             }
-            return 0;
+            return result;
+        }
+
+        #endregion
+
+        #region GetRow
+
+        private static Row GetRow(SheetData wsData, UInt32 rowIndex)
+        {
+            var row = wsData.Elements<Row>().
+            Where(r => r.RowIndex.Value == rowIndex).FirstOrDefault();
+            if (row == null)
+            {
+                row = new Row();
+                row.RowIndex = rowIndex;
+                wsData.Append(row);
+            }
+            return row;
+        }
+
+        #endregion
+
+        #region CreateCell
+
+        private static Cell CreateCell(Row row, String address)
+        {
+            Cell cellResult;
+            Cell refCell = null;
+
+            // Cells must be in sequential order according to CellReference. Determine where to insert the new cell.
+            foreach (Cell cell in row.Elements<Cell>())
+            {
+                if (string.Compare(cell.CellReference.Value, address, true) > 0)
+                {
+                    refCell = cell;
+                    break;
+                }
+            }
+
+            cellResult = new Cell();
+            cellResult.CellReference = address;
+
+            row.Append(cellResult);
+            return cellResult;
+        }
+
+        #endregion
+
+        #region InsertSharedStringItem
+
+        private static int InsertSharedStringItem(WorkbookPart wbPart, object value)
+        {
+            int index = 0;
+            bool found = false;
+            var stringTablePart = wbPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+
+            if (stringTablePart == null)
+            {
+
+                stringTablePart = wbPart.AddNewPart<SharedStringTablePart>();
+            }
+
+            var stringTable = stringTablePart.SharedStringTable;
+            if (stringTable == null)
+            {
+                stringTable = new SharedStringTable();
+            }
+
+            foreach (SharedStringItem item in stringTable.Elements<SharedStringItem>())
+            {
+                if (item.InnerText == value)
+                {
+                    found = true;
+                    break;
+                }
+                index += 1;
+            }
+
+            if (!found)
+            {
+                stringTable.AppendChild(new SharedStringItem(new Text((String)value)));
+                stringTable.Save();
+            }
+            return index;
+        }
+
+        #endregion
+
+        #region Renglones
+        private static SpreadsheetDocument Imprimir_02_02_renglones_0(DataSet dt, String ruta, String nombreArchivo, SpreadsheetDocument documento, ref int ultimaFila)
+        {
+            for (int i = 0; i < dt.Tables["XLSLISTA"].Rows.Count; i++)
+            {
+                DataRow r = dt.Tables["XLSLISTA"].Rows[i];
+                String fila = (i + 3).ToString();
+                documento = UpdateValue("A" + fila, r["co_art"], 11, CellValues.String, documento);
+                documento = UpdateValue("B" + fila, r["art_des"], 11, CellValues.String, documento);
+                documento = UpdateValue("C" + fila, r["des_uni"], 11, CellValues.String, documento);
+                documento = UpdateValue("D" + fila, r["cat_des"], 11, CellValues.String, documento);
+                documento = UpdateValue("E" + fila, r["Precio01"], 11, CellValues.Number, documento);
+                documento = UpdateValue("F" + fila, r["StockActual"], 11, CellValues.Number, documento);
+                documento = UpdateValue("G" + fila, Decimal.Zero, 11, CellValues.Number, documento);
+                documento = UpdateValue("H" + fila, r["co_art"], 11, CellValues.String, documento);
+
+                ultimaFila = i + 2;
+            }
+            return documento;
+        }
+        #endregion
+
+        #region UpdateValue
+        public static SpreadsheetDocument UpdateValue(string addressName, object value, UInt32Value styleIndex, CellValues tipo, SpreadsheetDocument documento)
+        {
+            WorkbookPart wbPart = documento.WorkbookPart;
+            Sheet sheet = wbPart.Workbook.Descendants<Sheet>().First();
+
+            if (sheet != null)
+            {
+                Worksheet ws = ((WorksheetPart)(wbPart.GetPartById(sheet.Id))).Worksheet;
+                Cell cell = InsertCellInWorksheet(ws, addressName);
+                String strValor = String.Empty;
+                int stringIndex;
+
+                switch (tipo)
+                {
+                    case CellValues.String:
+                        if (String.IsNullOrEmpty(Convert.ToString(value)))
+                            value = " ";
+                        stringIndex = InsertSharedStringItem(wbPart, value);
+                        strValor = stringIndex.ToString();
+                        tipo = CellValues.SharedString;
+                        //styleIndex = 10;
+                        break;
+                    case CellValues.Number:
+                        if (String.IsNullOrEmpty(Convert.ToString(value)))
+                            value = Decimal.Zero;
+                        CultureInfo innerCulture = new CultureInfo(CultureInfo.CurrentCulture.LCID);
+                        innerCulture.NumberFormat.NumberDecimalSeparator = ".";
+                        innerCulture.NumberFormat.NumberGroupSeparator = "";
+                        strValor = Convert.ToString((Decimal)value, innerCulture);
+                        //styleIndex = 5;
+                        break;
+                    case CellValues.Date:
+                        if (String.IsNullOrEmpty(Convert.ToString(value)))
+                            value = DateTime.MinValue;
+                        strValor = Convert.ToString(((DateTime)value).ToOADate());
+                        break;
+                }
+
+                cell.CellValue = new CellValue(strValor);
+                if (tipo != CellValues.Date)
+                    cell.DataType = new EnumValue<CellValues>(tipo);
+
+
+                if (styleIndex > 0)
+                    cell.StyleIndex = styleIndex;
+
+                ws.Save();
+
+            }
+
+            return documento;
         }
         #endregion
     }
